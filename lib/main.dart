260154d3670a3a24cff1cf80dd22ab5e4bdbf51e
Unkn8wn69 <79060957+Unkn8wn69@users.noreply.github.com>
2023-10-09 22:35:24 +0200

    Changed default to lightMode

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: OnBoardingScreen(),
-      theme: darkMode,
+      theme: lightMode,
       darkTheme: darkMode,
     );
   }
